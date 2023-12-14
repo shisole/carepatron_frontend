ca3726cc317c08e6827c14fdac74ef37a8dc9410
shisole <skj101001000101@gmail.com>
2023-12-14 12:20:08 +0800

    implementation and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import ClientsTable from './components/ClientsTable';
+import DefaultLayout from './components/Layout/DefaultLayout';
+import theme from './styles/theme';
+import { ThemeProvider } from 'styled-components';
+
 export const App = () => {
- return <>Frontend goes here</>
+ return <ThemeProvider theme={theme}>
+    <DefaultLayout>
+      <ClientsTable />
+    </DefaultLayout>
+ </ThemeProvider>
 }
