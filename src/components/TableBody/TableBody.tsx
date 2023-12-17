de680ca12aa172ad54814c22fbba4e2c86721090
shisole <skj101001000101@gmail.com>
2023-12-17 15:11:10 +0800

    cleanup

--- src/components/TableBody/TableBody.tsx
+++ src/components/TableBody/TableBody.tsx
@@ -9,20 +9,4 @@ interface TableBody extends TableHTMLAttributes<HTMLTableSectionElement> {}
 
 const TableBody: React.FC<TableBody> = ({ children, ...rest }) => (<TableBodyWrapper {...rest}>{children}</TableBodyWrapper>)
 
-
-// const TableBody = <T extends Record<string, GenericValue>>({ data, tableKeys }: TableBodyProps<T>) => {
-//     return <TableBodyWrapper>
-//         {
-//             data.map((tableData, index) =>
-//                 <TableRow key={index}>
-//                     {tableKeys.map((tableKey) =>
-//                             <TableCell key={tableKey}>
-//                                 {tableData[tableKey]}
-//                             </TableCell>)}
-//                 </TableRow>
-//             )
-//         }
-//     </TableBodyWrapper>
-// }
-
 export default TableBody;
